4408c988adf848b332dd6099f3b3effc5fc34d68
Pawan0897 <kumarpawan9041491@gmail.com>
2026-01-09 17:36:49 +0530

    latest

--- Components/SearchResultPage/HotelDetail/HotelDetailContent.jsx
+++ Components/SearchResultPage/HotelDetail/HotelDetailContent.jsx
@@ -64,7 +64,7 @@ export default function HotelDetailContent({ prices }) {
 
                                                 </div>
                                             )
-
+                                            git
                                         })
                                     }
                                 </div>
