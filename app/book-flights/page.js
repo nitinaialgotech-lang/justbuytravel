79c2bb432062bae0365e86d7c081e70ddcbfdaa7
Pawan0897 <kumarpawan9041491@gmail.com>
2026-01-23 12:50:49 +0530

    responsive

--- app/book-flights/page.js
+++ app/book-flights/page.js
@@ -2,8 +2,8 @@ import Header from '@/component/Header'
 import Book_Flight_Banner from '@/Components/Book-Flights/Book_Flight_Banner'
 import Flight_Iconic_Places from '@/Components/Book-Flights/Flight_Iconic_Places'
 import React, { Suspense } from 'react'
-import "../../style/responsive.css"
 import Helping_travel_Explore from '@/Components/Book-Flights/Helping_travel_Explore'
+import "../../style/responsive.css";
 export default function page() {
     return (
         <>
