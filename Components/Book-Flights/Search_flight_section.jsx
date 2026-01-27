5c1dd0dce3463e717125a2b5c4118e5ba33ad9a0
Pawan0897 <kumarpawan9041491@gmail.com>
2026-01-27 16:50:34 +0530

    update

--- Components/Book-Flights/Search_flight_section.jsx
+++ Components/Book-Flights/Search_flight_section.jsx
@@ -12,7 +12,7 @@ export default function Search_flight_section() {
                 <iframe
                     src="https://creator.expediagroup.com/widgets/search?program=us-expedia&network=pz&camref=1110lvIIC&lobs=flights"
                     width="100%"
-                    height="320"
+                    // height="320"
                     style={{ border: "none" }}
                     loading="lazy"
                     title="Expedia Flights"
